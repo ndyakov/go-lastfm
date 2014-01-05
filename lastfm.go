bef8e2991e7406d3b754a18216c69ad7320bffbe
Nedyalko Dyakov <me@ndyakov.eu>
2014-01-05 21:28:30 +0200

    Additional core types

--- lastfm.go
+++ lastfm.go
@@ -18,3 +18,19 @@ type LastfmDate struct {
 	Date string `xml:",chardata"`
 	UTS  int64  `xml:"uts,attr"`
 }
+
+type LastfmImage struct {
+	Size string `xml:"size,attr"`
+	URL  string `xml:",chardata"`
+}
+
+type LastfmStats struct {
+	Listeners int64 `xml:"listeners"`
+	Playes    int64 `xml:"plays"`
+}
+
+type LastfmWiki struct {
+	Published string `xml:"published"`
+	Summary   string `xml:"summary"`
+	Content   string `xml:"content"`
+}
