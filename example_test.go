3afff01afd8c4d8e6b2b132491c9240950839ece
Nedyalko Dyakov <me@ndyakov.eu>
2014-01-06 15:49:53 +0200

    Adding example test for New.

--- example_test.go
+++ example_test.go
@@ -3,9 +3,10 @@ package lastfm_test
 import (
 	"fmt"
 	"github.com/ndyakov/go-lastfm"
+	"reflect"
 )
 
-func ExampleArtist_GetTopTags() {
+func ExampleArtistClient_GetTopTags() {
 	lfm := lastfm.New("api_key_for_testing")
 	response, err := lfm.Artist.GetTopTags("Daft Punk", "", 0)
 	if err != nil {
@@ -14,4 +15,11 @@ func ExampleArtist_GetTopTags() {
 	fmt.Printf("Number of Top Tags %v\n", len(response.TopTags))
 	// Output:
 	// Number of Top Tags 100
+}
+
+func ExampleLastFM_New() {
+	lfm := lastfm.New("api_key_for_testing")
+	fmt.Printf("The type is %v\n", reflect.TypeOf(lfm))
+	// Output:
+	// The type is *lastfm.LastFM
 }
