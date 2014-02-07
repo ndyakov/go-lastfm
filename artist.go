791a5d7b76e7d595063f02ef275acde75454e19c
Nedyalko Dyakov <me@ndyakov.eu>
2014-02-07 19:13:44 +0200

    Add comments to Artist section.

--- artist.go
+++ artist.go
@@ -33,6 +33,7 @@ func (c *ArtistClient) prepareQuery(name, mbid string, autocorrect int) (query m
 
 // Get top tags for Artist.
 // Returns TopTagsResponse structure or error.
+// Be careful, there may be error returned from the parsing as well.
 func (c *ArtistClient) GetTopTags(name, mbid string, autocorrect int) (response TopTagsResponse, err error) {
 	method := "artist.getTopTags"
 	query := c.prepareQuery(name, mbid, autocorrect)
@@ -57,6 +58,9 @@ func (c *ArtistClient) GetTopTags(name, mbid string, autocorrect int) (response 
 	return
 }
 
+// Get tags for Artist who is in users directory.
+// Returns TagsResponse structure or error.
+// Be careful, there may be error returned from the parsing as well.
 func (c *ArtistClient) GetTags(name, mbid string, autocorrect int, user string) (response TagsResponse, err error) {
 	method := "artist.getTags"
 	query := c.prepareQuery(name, mbid, autocorrect)
@@ -82,6 +86,9 @@ func (c *ArtistClient) GetTags(name, mbid string, autocorrect int, user string) 
 	return
 }
 
+// Get top Albums for Artist.
+// Returns TopAlbumsResponse or error.
+// Be careful, there may be error returned from the parsing as well.
 func (c *ArtistClient) GetTopAlbums(name, mbid string, autocorrect, page, limit int) (response TopAlbumsResponse, err error) {
 	method := "artist.getTopAlbums"
 	query := c.prepareQuery(name, mbid, autocorrect)
@@ -115,6 +122,9 @@ func (c *ArtistClient) GetTopAlbums(name, mbid string, autocorrect, page, limit 
 	return
 }
 
+// Get top fans for Artist.
+// Returns TopFansResponse or error.
+// Be careful, there may be error returned from the parsing as well.
 func (c *ArtistClient) GetTopFans(name, mbid string, autocorrect int) (response TopFansResponse, err error) {
 	method := "artist.getTopFans"
 	query := c.prepareQuery(name, mbid, autocorrect)
@@ -139,6 +149,9 @@ func (c *ArtistClient) GetTopFans(name, mbid string, autocorrect int) (response 
 	return
 }
 
+// Get top tracks for Artist.
+// Returns TopTracksResponse or error.
+// Be careful, there may be error returned from the parsing as well.
 func (c *ArtistClient) GetTopTracks(name, mbid string, autocorrect, page, limit int) (response TopTracksResponse, err error) {
 	method := "artist.getTopTracks"
 	query := c.prepareQuery(name, mbid, autocorrect)
@@ -172,7 +185,8 @@ func (c *ArtistClient) GetTopTracks(name, mbid string, autocorrect, page, limit 
 	return
 }
 
-// Search for Artist by given name
+// Search for Artist by given name.
+// Returns ArtistSearchResponse or error.
 func (c *ArtistClient) Search(name string, page, limit int) (response ArtistSearchResponse, err error) {
 	method := "artist.Search"
 	query := make(map[string]string)
@@ -233,6 +247,9 @@ func (c *ArtistClient) GetSimilar(name string, mbid string, autocorrect int) (re
 	return
 }
 
+// Get info for Artist with given name.
+// Returns ArtistInfoResponse or error.
+// Response data is actually in response.Artist.
 func (c *ArtistClient) GetInfo(name, mbid string, autocorrect int) (response ArtistInfoResponse, err error) {
 	method := "artist.getInfo"
 	query := c.prepareQuery(name, mbid, autocorrect)
--- artist_responses.go
+++ artist_responses.go
@@ -1,9 +1,12 @@
 package lastfm
 
+// ArtistBioResponse part of ArtistResponse
+// can be found in artist.getInfo`s response.
 type ArtistBioResponse struct {
 	LastfmWikiResponse
 }
 
+// ArtistResponse, used where <artist> tag is present.
 type ArtistResponse struct {
 	Name           string                `xml:"name"`
 	MBID           string                `xml:"mbid"`
@@ -16,6 +19,7 @@ type ArtistResponse struct {
 	Bio            ArtistBioResponse     `xml:"bio"`
 }
 
+// ArtistInfoResponse, used for artist.getInfo request.
 type ArtistInfoResponse struct {
 	LastfmStatusResponse
 	Artist ArtistResponse `xml:"artist"`
@@ -26,17 +30,20 @@ type ArtistCorrectionResponse struct {
 	Corrections []ArtistResponse `xml:"corrections>correction>artist"`
 }
 
+// ArtistSimilarResponse, used where <similarartists> is present.
 type ArtistSimilarResponse struct {
 	LastfmStatusResponse
 	SimilarArtists []ArtistResponse `xml:"similarartists>artist"`
 }
 
+// ArtistSearchResponse, used for artist.Search request.
 type ArtistSearchResponse struct {
 	LastfmStatusResponse
 	LastfmOpenSearchResponse
 	ArtistMatches []ArtistResponse `xml:"artistmatches>artist"`
 }
 
+// TopArtistsResponse, used where <topartists> tag is present.
 type TopArtistsResponse struct {
 	LastfmStatusResponse
 	TopArtists []ArtistResponse `xml:"topartists>artist"`
