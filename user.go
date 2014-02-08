89667a5dddf1bf4ed1c24d32e6e97eb5979d29d1
Nedyalko Dyakov <me@ndyakov.eu>
2014-02-08 22:31:03 +0200

    Add comments in User section.

--- user.go
+++ user.go
@@ -13,6 +13,8 @@ type UserClient struct {
 	Client
 }
 
+// Used for most of the UserClient methods to prepare the query.
+// Returns map[string]string that can be used with lastfm's makeRequest method.
 func (c *UserClient) prepareQuery(user string, page, limit int) (query map[string]string) {
 	query = make(map[string]string)
 	query["user"] = user
@@ -28,6 +30,8 @@ func (c *UserClient) prepareQuery(user string, page, limit int) (query map[strin
 	return
 }
 
+// Get full user information for some user.
+// Returns UserInfoResponse or error.
 func (c *UserClient) GetInfo(user string) (response UserInfoResponse, err error) {
 	method := "user.getInfo"
 	query := c.prepareQuery(user, 0, 0)
@@ -52,6 +56,9 @@ func (c *UserClient) GetInfo(user string) (response UserInfoResponse, err error)
 	return
 }
 
+// Get loved tracks for some user.
+// You can specify the limit and the page for the result.
+// Returns LovedTracksResponse or error.
 func (c *UserClient) GetLovedTracks(user string, page, limit int) (response LovedTracksResponse, err error) {
 	method := "user.getLovedTracks"
 	query := c.prepareQuery(user, page, limit)
@@ -76,6 +83,9 @@ func (c *UserClient) GetLovedTracks(user string, page, limit int) (response Love
 	return
 }
 
+// Get neighbours for some user. You may specify the limit of
+// returned users.
+// Returns NeighboursResponse or error.
 func (c *UserClient) GetNeighbours(user string, limit int) (response NeighboursResponse, err error) {
 	method := "user.getNeighbours"
 	query := c.prepareQuery(user, 0, limit)
@@ -100,6 +110,10 @@ func (c *UserClient) GetNeighbours(user string, limit int) (response NeighboursR
 	return
 }
 
+// Ger recent tracks listened by some user.
+// You may specify the period (from, to) as UNIX timestamp.
+// Also you may pass page and limit for the request.
+// Returns RecentTracksResponse or error.
 func (c *UserClient) GetRecentTracks(user string, page, limit int, from, to int64) (response RecentTracksResponse, err error) {
 	method := "user.getRecentTracks"
 	query := c.prepareQuery(user, page, limit)
@@ -134,6 +148,12 @@ func (c *UserClient) GetRecentTracks(user string, page, limit int, from, to int6
 	return
 }
 
+// Get top albums for some user.
+// You may specify page and limit for the request.
+// Also you may pass the period of the search.
+// For more informacion about valid periods refer to
+// lastfm's api documentation.
+// Returns TopAlbumsResponse or error.
 func (c *UserClient) GetTopAlbums(user, period string, page, limit int) (response TopAlbumsResponse, err error) {
 	method := "user.getTopAlbums"
 	query := c.prepareQuery(user, page, limit)
@@ -163,6 +183,12 @@ func (c *UserClient) GetTopAlbums(user, period string, page, limit int) (respons
 	return
 }
 
+// Get top artists for some user.
+// You may specify page and limit for the request.
+// Also you may pass the period of the search.
+// For more informacion about valid periods refer to
+// lastfm's api documentation.
+// Returns TopArtistsResponse or error.
 func (c *UserClient) GetTopArtists(user, period string, page, limit int) (response TopArtistsResponse, err error) {
 	method := "user.getTopArtists"
 	query := c.prepareQuery(user, page, limit)
@@ -192,6 +218,12 @@ func (c *UserClient) GetTopArtists(user, period string, page, limit int) (respon
 	return
 }
 
+// Get top tracks listened by some user.
+// You may specify page and limit for the request.
+// Also you may pass the period of the search.
+// For more informacion about valid periods refer to
+// lastfm's api documentation.
+// Returns TopTracksResponse or error.
 func (c *UserClient) GetTopTracks(user, period string, page, limit int) (response TopTracksResponse, err error) {
 	method := "user.getTopTracks"
 	query := c.prepareQuery(user, page, limit)
@@ -221,6 +253,9 @@ func (c *UserClient) GetTopTracks(user, period string, page, limit int) (respons
 	return
 }
 
+// Get top tags by some user.
+// You may specify the limit of tags to be requested.
+// Returns TopTagsResponse or error.
 func (c *UserClient) GetTopTags(user string, limit int) (response TopTagsResponse, err error) {
 	method := "user.getTopTags"
 	query := c.prepareQuery(user, 0, limit)
--- user_responses.go
+++ user_responses.go
@@ -1,5 +1,7 @@
 package lastfm
 
+// UserResponse
+// Used where <user> tag is present.
 type UserResponse struct {
 	ID         int64                 `xml:"id"`
 	Name       string                `xml:"name"`
@@ -16,25 +18,36 @@ type UserResponse struct {
 	Registered LastfmDateResponse    `xml:"registered"`
 }
 
+// UserInfoResponse
+// Used for the user.getInfo's response.
 type UserInfoResponse struct {
 	LastfmStatusResponse
 	User UserResponse `xml:"user"`
 }
 
+// FanResponse
+// Part of TopFansResponse.
 type FanResponse struct {
 	UserResponse
 	Weight int64 `xml:"weight"`
 }
 
+// TopFansResponse
+// Used where <topfans> tag is present.
 type TopFansResponse struct {
 	LastfmStatusResponse
 	TopFans []FanResponse `xml:"topfans>user"`
 }
 
+// NeighbourResponse
+// Part of NeighboursResponse.
 type NeighbourResponse struct {
 	UserResponse
 	Match float64 `xml:"match"`
 }
+
+// NeighboursResponse
+// Used where <neighbours> tag is present.
 type NeighboursResponse struct {
 	LastfmStatusResponse
 	Neighbours []NeighbourResponse `xml:"neighbours>user"`
